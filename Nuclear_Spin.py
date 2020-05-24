e06f9d4d6f4713e17a6dffb20f854e5b6bee9e53
Davide Candoli <davide.candoli@studio.unibo.it>
2020-05-24 12:40:01 +0200

    Added file Nuclear_Spin.py, which is intended to host the definition of the class with the same name. Up to now, only a stub class has been defined.

---
+++ Nuclear_Spin.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+
+from Operators import Operator, Density_Matrix, \
+                      Observable, Random_Operator, \
+                      Random_Observable, Random_Density_Matrix, \
+                      Commutator, \
+                      Magnus_Expansion_1st_Term, \
+                      Magnus_Expansion_2nd_Term
+
+class Nuclear_Spin:
+    pass
