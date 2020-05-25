ffcff9c124127c3981a59d79e6d4371aad80799f
Davide Candoli <davide.candoli@studio.unibo.it>
2020-05-25 17:53:46 +0200

    Written the implementation of the function H_Quadrupole, associated with the quadrupolar Hamiltonian, and introduced the stub functions V0, V1, V2 which are called in the scope of the former.

--- Hamiltonians.py
+++ Hamiltonians.py
@@ -10,11 +10,36 @@ from Operators import Operator, Density_Matrix, \
 
 from Nuclear_Spin import Nuclear_Spin
 
+# Returns the Observable object representing the Hamiltonian for the Zeeman interaction with an external
+# static field
+def H_Zeeman(spin, theta_z, phi_z, H_0):
+    if H_0<0: raise ValueError("The modulus of the magnetic field must be a non-negative quantity")
+    h_Zeeman = -spin.gyromagnetic_ratio*H_0* \
+                (math.sin(theta_z)*math.cos(phi_z)*spin.I['x'] + \
+                 math.sin(theta_z)*math.sin(phi_z)*spin.I['y'] + \
+                 math.cos(theta_z)*spin.I['z'])
+    return Observable(h_Zeeman.matrix)
 
-def H_Zeeman():
+def H_Quadrupole(spin, eQ, eq, eta, alpha, beta, gamma):
+    h_quadrupole = eQ/(spin.quantum_number*(2*spin.quantum_number-1))* \
+                   ((1/2)*(3*(spin.I['z']**2)-spin.quantum_number*(spin.quantum_number+1))* \
+                    V0(eq, eta, alpha, beta, gamma) + \
+                    (math.sqrt(6)/4)* \
+                    ((spin.I['z']*spin.I['+'] + spin.I['+']*spin.I['z'])* \
+                     V1(-1, eq, eta, alpha, beta, gamma) - \
+                     (spin.I['z']*spin.I['-'] + spin.I['-']*spin.I['z'])* \
+                     V1(+1, eq, eta, alpha, beta, gamma) + \
+                     (spin.I['+']**2)*V2(-2, eq, eta, alpha, beta, gamma) + \
+                     (spin.I['-']**2)*V2(+2, eq, eta, alpha, beta, gamma)))
+    return h_quadrupole
+
+def V0(eq, eta, alpha, beta, gamma):
     pass
 
-def H_Quadrupole():
+def V1(sign, eq, eta, alpha, beta, gamma):
+    pass
+
+def V2(sign, eq, eta, alpha, beta, gamma):
     pass
 
 def H_Pulse():
