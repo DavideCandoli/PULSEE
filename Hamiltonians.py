64e49c5ff15b2bb2a6e67e4127f3cebae32b519c
Davide Candoli <davide.candoli@studio.unibo.it>
2020-05-25 12:15:52 +0200

    Added file Hamiltonians.py, which is dedicated to the definition of the functions which generate the terms of the Hamiltonian of the system under study.

---
+++ Hamiltonians.py
@@ -0,0 +1,21 @@
+import math
+import numpy as np
+
+from Operators import Operator, Density_Matrix, \
+                      Observable, Random_Operator, \
+                      Random_Observable, Random_Density_Matrix, \
+                      Commutator, \
+                      Magnus_Expansion_1st_Term, \
+                      Magnus_Expansion_2nd_Term
+
+from Nuclear_Spin import Nuclear_Spin
+
+
+def H_Zeeman():
+    pass
+
+def H_Quadrupole():
+    pass
+
+def H_Pulse():
+    pass
