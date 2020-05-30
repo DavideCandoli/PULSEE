e353d7b2f215d8110d92e31823d1e11170d16267
Davide Candoli <davide.candoli@studio.unibo.it>
2020-05-30 12:11:55 +0200

    Implemented the main function of the simulation software, Simulate, which runs the whole simulation. So far, only the evolution of the density matrix has been implemented. In file Test_Simulation, it has been checked that the function Simulate works fine, but no testing has been made on the correctness of the results yet.

--- Simulation.py
+++ Simulation.py
@@ -6,88 +6,52 @@ from Operators import Operator, Density_Matrix, \
                       Random_Observable, Random_Density_Matrix, \
                       Commutator, \
                       Magnus_Expansion_1st_Term, \
-                      Magnus_Expansion_2nd_Term
+                      Magnus_Expansion_2nd_Term, \
+                      Canonical_Density_Matrix
 
 from Nuclear_Spin import Nuclear_Spin
 
 from Hamiltonians import H_Zeeman, H_Quadrupole, \
-                         H_Pulse, V0, V1, V2
+                         H_Single_Mode_Pulse, \
+                         H_Multiple_Mode_Pulse, \
+                         H_Pulse_IP, \
+                         V0, V1, V2
 
-
-# This instance of the Nuclear_Spin class is the basic object underpinning all the calculations in the following simulation, so it is introduced as a global variable
-#spin = Nuclear_Spin()
-
-# Main function of the program, it runs every step of the simulation
-#def Simulate():
-#    spin.set_quantum_number(Input_Spin_Quantum_Number())
-#    pulse_time = Input_Pulse_Time()
-#    initial_density_matrix = Compute_Canonical_Density_Matrix()
-#    evolved_density_matrix = Evolve(initial_density_matrix, pulse_time)
-#    return evolved_density_matrix
-
-# Drives the evolution of the state of the ensemble of nuclear spins
-#def Evolve(density_matrix_0, T):
-#    h1_IP_Magnus0th = Compute_Magnus_0th_Term(H1_IP, T)
-#    h1_IP_Magnus1st = Compute_Magnus_1st_Term(H1_IP, T)
-#    h1_IP_average = h1_IP_Magnus0th + h1_IP_Magnus1st
-#    density_matrix_IP_T = Unitary_Sandwich(density_matrix_0, -h1_IP_average*T)
-#    density_matrix_T = Interaction_Picture(density_matrix_IP_T, H0, T, invert=True)
-#    return density_matrix_T
-
-# Asks for the spin quantum number as an input from terminal
-#def Input_Spin_Quantum_Number():
-#    return 1.
-
-# Asks for the duration of the pulse as an input from terminal
-#def Input_Pulse_Time():
-#    return 1.
-
-# Calculates the expression of the density matrix associated to a state of the ensemble of nuclear spins at thermal equilibrium
-#def Compute_Canonical_Density_Matrix():
-#    canonical_density_matrix = np.zeros((spin.d, spin.d), dtype=complex)
-#    for m in range(spin.d):
-#        canonical_density_matrix[m][m] = 1/(spin.d)
-#    return canonical_density_matrix
-
-# Calculates the 0th term of Magnus expansion of the given Hamiltonian
-#def Compute_Magnus_0th_Term(Hamiltonian, time_interval):
-#    magnus0th = np.zeros((spin.d, spin.d))
-#    return magnus0th
-
-# Calculates the 1st term of Magnus expansion of the given Hamiltonian
-#def Compute_Magnus_1st_Term(Hamiltonian, time_interval):
-#    magnus1st = np.zeros((spin.d, spin.d))
-#    return magnus1st
-
-# Performs the matrix product U x M x U^(-1), where M stands for matrix and U is the complex exponential of exponent_operator
-#def Unitary_Sandwich(matrix, exponent_operator):
-#    return matrix
-
-# Casts the input operator matrix into the interaction picture with respect to the Unperturbed_Hamiltonian at instant time. When invert is true, the function performs the inverse operation (receives the matrix in the interaction picture and returns its form in the standard one)
-#def Interaction_Picture(matrix, Unperturbed_Hamiltonian, time=1., invert=False):
-#    return matrix
-
-# Stationary Hamiltonian of the system
-#def H0():
-#    return H_quadrupole + H_Zeeman
-
-# Quadrupolar interaction term of the Hamiltonian
-#def H_quadrupole():
-#    h_quadrupole = np.zeros((spin.d, spin.d))
-#    return h_quadrupole
-
-# Zeeman term of the Hamiltonian 
-#def H_Zeeman():
-#    h_Zeeman = np.zeros((spin.d, spin.d))
-#    return h_Zeeman
-
-# Perturbation Hamiltonian (electromagnetic pulse)
-#def H1(time):
-#    h1 = np.zeros((spin.d, spin.d))
-#    return h1
-
-# Perturbation Hamiltonian cast in the interaction picture
-#def H1_IP(time):
-#    Interaction_Picture(H1(time), H0, time)
+# Function that runs the simulation
+def Simulate(s, gyro_ratio, \
+             theta_z, phi_z, H_0, \
+             eQ, eq, eta, alpha_q, beta_q, gamma_q, \
+             temperature, \
+             mode, \
+             pulse_duration):
     
+    # Nuclear spin under study
+    spin = Nuclear_Spin(s, gyro_ratio)
     
+    # Computes the unperturbed Hamiltonian of the system, namely the sum of the Zeeman and Quadrupole
+    # contribution
+    h_unperturbed = H_Zeeman(spin, theta_z, phi_z, H_0) + \
+                    H_Quadrupole(spin, eQ, eq, eta, alpha_q, beta_q, gamma_q)
+    
+    # Density matrix of the system at time t=0, when the ensemble of spins is considered at equilibrium
+    dm_initial = Canonical_Density_Matrix(h_unperturbed, temperature)
+    
+    # Sampling of the time-dependent term of the Hamiltonian representing the coupling with the
+    # electromagnetic pulse (already cast in the interaction picture)
+    times, time_step = np.linspace(0, pulse_duration, num=int(pulse_duration*100), retstep=True)
+    h_pulse_ip = []
+    for t in times:
+        h_pulse_ip.append(H_Pulse_IP(spin, mode, t, h_unperturbed))
+        
+    # Evaluation of the 1st and 2nd terms of the Magnus expansion for the pulse Hamiltonian in the
+    # interaction picture
+    magnus_1st = Magnus_Expansion_1st_Term(h_pulse_ip, time_step)
+    magnus_2nd = Magnus_Expansion_2nd_Term(h_pulse_ip, time_step)
+    
+    # Density matrix of the system after evolution under the action of the pulse
+    dm_evolved_ip = dm_initial.sim_trans(-(magnus_1st+magnus_2nd), exp=True)
+    
+    # Evolved density matrix cast back in the interaction picture
+    dm_evolved = dm_evolved_ip.interaction_picture(h_unperturbed, pulse_duration, invert=True)
+    
+    return Density_Matrix(dm_evolved.matrix)
--- Test_Simulation.py
+++ Test_Simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import math
 
 import hypothesis.strategies as st
@@ -13,49 +14,17 @@ from Operators import Operator, Density_Matrix, \
 
 from Nuclear_Spin import Nuclear_Spin
 
-from Hamiltonians import H_Zeeman, H_Quadrupole, \
-                         H_Pulse, V0, V1, V2
+from Hamiltonians import H_Zeeman, H_Quadrupole, H_Pulse_IP
 
+from Simulation import Simulate
 
-#def Generate_Random_Operator():
-#    return np.random.random_sample((spin.d, spin.d))*256 - 128
-
-#def Generate_Random_Density_Matrix():
-#    spectrum = np.random.random_sample(spin.d)*256 - 128
-#    normalised_spectrum = spectrum / np.sum(spectrum)
-#    random_matrix = np.zeros((spin.d, spin.d))
-#    for m in range(spin.d):
-#        random_matrix[m] = spectrum[m]
-#    random_exponent_operator = Generate_Random_Operator()
-#    return Unitary_Sandwich(random_matrix, random_exponent_operator)
-
-#def test_Unitary_Sandwich_Conserves_Trace():
-#    for n_trial in range(64):
-#        test_matrix = Generate_Random_Density_Matrix()
-#        random_operator = Generate_Random_Operator()
-#        transformed_test_matrix = Unitary_Sandwich(test_matrix, random_operator)
-#        assert math.isclose(Trace(transformed_test_matrix), Trace(test_matrix), rel_tol=1e-10)
-        
-#def test_Interaction_Picture_Reversibility():
-#    for n_trial in range(64):
-#        test_matrix = Generate_Random_Density_Matrix()
-#        test_matrix_IP = Interaction_Picture(test_matrix, Generate_Random_Operator)
-#        test_matrix_restored = Interaction_Picture(test_matrix_IP, Generate_Random_Operator, invert=True)
-#        tautology = np.all(np.isclose(test_matrix, test_matrix_restored, rtol=1e-10))
-#        assert tautology
-        
-#def test_H_quadrupole_Hermitianity():
-#    h_q = H_quadrupole()
-#    h_q_dagger = np.conj(h_q.transpose())
-#    assert np.all(np.isclose(h_q, h_q_dagger, rtol=1e-10))
-    
-#def test_H_Zeeman_Hermitianity():
-#    h_z = H_Zeeman()
-#    h_z_dagger = np.conj(h_z.transpose())
-#    assert np.all(np.isclose(h_z, h_z_dagger, rtol=1e-10))
-    
-#@given(time=st.floats())
-#def test_H1_Hermitianity(time):
-#    h1 = H1(time)
-#    h1_dagger = np.conj(h1.transpose())
-#    assert np.all(np.isclose(h1, h1_dagger, rtol=1e-10))
+# Checks if the simulation works, doesn't check wrong output
+def test_Generic_Simulation():
+    mode = pd.DataFrame([(5., 10., 0., math.pi/2, 0.)], 
+                        columns=['frequency', 'amplitude', 'phase', 'theta', 'phi'])
+    Simulate(1, 1., \
+             0, 0, 5., \
+             2., 2., 0.5, math.pi/2, math.pi/2, math.pi/2, \
+             300, \
+             mode, \
+             5.)
