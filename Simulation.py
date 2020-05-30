c4dbfb7a61cdb818a306d9f2bdfecb0959269f5d
Davide Candoli <davide.candoli@studio.unibo.it>
2020-05-30 16:09:08 +0200

    Changed the implementation of Simulate(), which now prints as outputs the eigenvalues of the unperturbed Hamiltonian, the initial density matrix and calls for a newly added method Evolve() in order to generate the evolved density matrix.

--- Simulation.py
+++ Simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import math
 
 from Operators import Operator, Density_Matrix, \
@@ -28,30 +29,52 @@ def Simulate(s, gyro_ratio, \
     # Nuclear spin under study
     spin = Nuclear_Spin(s, gyro_ratio)
     
-    # Computes the unperturbed Hamiltonian of the system, namely the sum of the Zeeman and Quadrupole
-    # contribution
-    h_unperturbed = H_Zeeman(spin, theta_z, phi_z, H_0) + \
-                    H_Quadrupole(spin, eQ, eq, eta, alpha_q, beta_q, gamma_q)
+    # Zeeman term of the Hamiltonian
+    h_zeeman = H_Zeeman(spin, theta_z, phi_z, H_0)
     
-    # Density matrix of the system at time t=0, when the ensemble of spins is considered at equilibrium
+    # Quadrupole term of the Hamiltonian
+    h_quadrupole = H_Quadrupole(spin, eQ, eq, eta, alpha_q, beta_q, gamma_q)
+    
+    # Computes the unperturbed Hamiltonian of the system, namely the sum of the Zeeman and quadrupole
+    # contributions
+    h_unperturbed = Observable(h_zeeman.matrix + h_quadrupole.matrix)
+    
+    # Energy levels of the unperturbed Hamiltonian
+    energy_spectrum = h_unperturbed.eigenvalues()
+    
+    print("Energy eigenvalues of the Zeeman + quadrupole Hamiltonian = %r" % (energy_spectrum))
+    
+    # Density matrix of the system at time t=0, when the ensemble of spins is at equilibrium
     dm_initial = Canonical_Density_Matrix(h_unperturbed, temperature)
     
+    print("Initial density matrix = \n %r" % (dm_initial.matrix))
+    
+    # Evolves the density matrix under the action of the specified pulse through the time interval
+    # pulse_duration
+    dm_evolved = Evolve(spin, dm_initial, h_unperturbed, mode, pulse_duration)
+    
+    print("Evolved density matrix = \n %r" % (dm_evolved.matrix))
+
+    
+def Evolve(spin, dm_0, h_0, mode, T):
+    
     # Sampling of the time-dependent term of the Hamiltonian representing the coupling with the
-    # electromagnetic pulse (already cast in the interaction picture)
-    times, time_step = np.linspace(0, pulse_duration, num=int(pulse_duration*100), retstep=True)
+    # electromagnetic pulse (already cast in the interaction picture) in the time window [0, T]
+    times, time_step = np.linspace(0, T, num=int(T*100), retstep=True)
     h_pulse_ip = []
     for t in times:
-        h_pulse_ip.append(H_Pulse_IP(spin, mode, t, h_unperturbed))
+        h_pulse_ip.append(H_Pulse_IP(spin, mode, t, h_0))
         
     # Evaluation of the 1st and 2nd terms of the Magnus expansion for the pulse Hamiltonian in the
     # interaction picture
     magnus_1st = Magnus_Expansion_1st_Term(h_pulse_ip, time_step)
     magnus_2nd = Magnus_Expansion_2nd_Term(h_pulse_ip, time_step)
     
-    # Density matrix of the system after evolution under the action of the pulse
-    dm_evolved_ip = dm_initial.sim_trans(-(magnus_1st+magnus_2nd), exp=True)
+    # Density matrix of the system after evolution under the action of the pulse (in the interaction)
+    # picture
+    dm_T_ip = dm_0.sim_trans(-(magnus_1st+magnus_2nd), exp=True)
     
     # Evolved density matrix cast back in the interaction picture
-    dm_evolved = dm_evolved_ip.interaction_picture(h_unperturbed, pulse_duration, invert=True)
+    dm_T = dm_T_ip.interaction_picture(h_0, T, invert=True)
     
-    return Density_Matrix(dm_evolved.matrix)
+    return Density_Matrix(dm_T.matrix)
--- Test_Simulation.py
+++ Test_Simulation.py
@@ -18,13 +18,13 @@ from Hamiltonians import H_Zeeman, H_Quadrupole, H_Pulse_IP
 
 from Simulation import Simulate
 
-# Checks if the simulation works, doesn't check wrong output
+# Checks if the simulation works, doesn't check if there is wrong output
 def test_Generic_Simulation():
     mode = pd.DataFrame([(5., 10., 0., math.pi/2, 0.)], 
                         columns=['frequency', 'amplitude', 'phase', 'theta', 'phi'])
-    Simulate(1, 1., \
-             0, 0, 5., \
-             2., 2., 0.5, math.pi/2, math.pi/2, math.pi/2, \
-             300, \
-             mode, \
-             5.)
+    Simulate(s=1, gyro_ratio=1., \
+             theta_z=0, phi_z=0, H_0=5., \
+             eQ=2., eq=2., eta=0.5, alpha_q=math.pi/2, beta_q=math.pi/2, gamma_q=math.pi/2, \
+             temperature=300, \
+             mode=mode, \
+             pulse_duration=5.)
