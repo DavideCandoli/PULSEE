40943629a93584bc6bdcf60cce4e51e3b51ad6fb
Davide Candoli <davide.candoli@studio.unibo.it>
2020-06-01 10:24:39 +0200

    Assigned the task of computing the energy eigenvalues of the unperturbed Hamiltonian to a new function called Energy_Spectrum. Also, transferred all the print commands into the scopes of the proper subfunctions Energy_Spectrum and Evolve. Eventually, added the possibility to skip the execution of the Evolve function inside Simulate by passing a pulse_time=0: the returned density matrix will be the same as the initial one (dm_0).

--- Simulation.py
+++ Simulation.py
@@ -22,48 +22,48 @@ from Hamiltonians import H_Zeeman, H_Quadrupole, \
 def Simulate(spin_par, zeem_par, quad_par, mode, temperature, pulse_time):
     
     # Nuclear spin under study
-    spin_par = spin_par.iloc[0]
     spin = Nuclear_Spin(spin_par['quantum number'], \
                         spin_par['gyromagnetic ratio'])
     
     # Zeeman term of the Hamiltonian
-    zeem_par = zeem_par.iloc[0]
-    h_zeeman = H_Zeeman(spin, zeem_par['theta'], \
-                              zeem_par['phi'], \
+    h_zeeman = H_Zeeman(spin, zeem_par['theta_z'], \
+                              zeem_par['phi_z'], \
                               zeem_par['field magnitude'])
     
     # Quadrupole term of the Hamiltonian
-    quad_par = quad_par.iloc[0]
     h_quadrupole = H_Quadrupole(spin, quad_par['coupling constant'], \
                                       quad_par['asymmetry parameter'], \
-                                      quad_par['alpha'], \
-                                      quad_par['beta'], \
-                                      quad_par['gamma'])
+                                      quad_par['alpha_q'], \
+                                      quad_par['beta_q'], \
+                                      quad_par['gamma_q'])
     
     # Computes the unperturbed Hamiltonian of the system, namely the sum of the Zeeman and quadrupole
     # contributions
     h_unperturbed = Observable(h_zeeman.matrix + h_quadrupole.matrix)
     
-    # Energy levels of the unperturbed Hamiltonian
-    energy_spectrum = h_unperturbed.eigenvalues()
-    
-    print("Energy eigenvalues of the Zeeman + quadrupole Hamiltonian = %r" % (energy_spectrum))
+    Energy_Spectrum(h_unperturbed)
     
     # Density matrix of the system at time t=0, when the ensemble of spins is at equilibrium
     dm_initial = Canonical_Density_Matrix(h_unperturbed, temperature)
     
-    print("Initial density matrix = \n %r" % (dm_initial.matrix))
+    print("Initial density matrix = \n %r" % (dm_initial.matrix), '\n')
     
     # Evolves the density matrix under the action of the specified pulse through the time interval
     # pulse_time
-    dm_evolved = Evolve(spin, dm_initial, h_unperturbed, mode, pulse_time)
-    
-    print("Evolved density matrix = \n %r" % (dm_evolved.matrix))
+    Evolve(spin, dm_initial, h_unperturbed, mode, pulse_time)
 
+    
+def Energy_Spectrum(h_0):
+    energy_spectrum = h_0.eigenvalues()
+    print("Energy eigenvalues of the Zeeman + quadrupole Hamiltonian = %r" % (energy_spectrum), '\n')
+    return energy_spectrum
+    
 
 # Computes the density matrix of the system after the application of a desired pulse for a given time,
 # given the initial preparation of the ensemble
 def Evolve(spin, dm_0, h_0, mode, T):
+    
+    if T == 0: return dm_0
     
     # Sampling of the time-dependent term of the Hamiltonian representing the coupling with the
     # electromagnetic pulse (already cast in the interaction picture) in the time window [0, T]
@@ -77,11 +77,13 @@ def Evolve(spin, dm_0, h_0, mode, T):
     magnus_1st = Magnus_Expansion_1st_Term(h_pulse_ip, time_step)
     magnus_2nd = Magnus_Expansion_2nd_Term(h_pulse_ip, time_step)
     
-    # Density matrix of the system after evolution under the action of the pulse (in the interaction)
-    # picture
+    # Density matrix of the system after evolution under the action of the pulse (in the interaction
+    # picture)
     dm_T_ip = dm_0.sim_trans(-(magnus_1st+magnus_2nd), exp=True)
     
-    # Evolved density matrix cast back in the interaction picture
+    # Evolved density matrix cast back in the Schroedinger picture
     dm_T = dm_T_ip.interaction_picture(h_0, T, invert=True)
+        
+    print("Evolved density matrix = \n %r" % (dm_T.matrix), '\n')
     
     return Density_Matrix(dm_T.matrix)
