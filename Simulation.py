32b7136a01fb15973e873539c8037299fe956f6f
Davide Candoli <davide.candoli@studio.unibo.it>
2020-06-01 16:12:17 +0200

    Removed the parts of the code which printed the energies of the system and the frequencies of the possible transitions. In this way, the function Simulate has the only purpose of returning the evolved matrix (as in the reference code by Possa et al.).

--- Simulation.py
+++ Simulation.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import math
 
+import sys
+sys.path.append('../')
+
 from Operators import Operator, Density_Matrix, \
                       Observable, Random_Operator, \
                       Random_Observable, Random_Density_Matrix, \
@@ -41,7 +44,11 @@ def Simulate(spin_par, zeem_par, quad_par, mode, temperature, pulse_time):
     # contributions
     h_unperturbed = Observable(h_zeeman.matrix + h_quadrupole.matrix)
     
-    Energy_Spectrum(h_unperturbed)
+    # Computes the energy eigenvalues of the unperturbed Hamiltonian
+    energy_spectrum = Energy_Spectrum(h_unperturbed)
+    
+    # Computes the frequencies of the transitions between energy levels
+    transition_frequencies = Frequency_Spectrum(energy_spectrum)
     
     # Density matrix of the system at time t=0, when the ensemble of spins is at equilibrium
     dm_initial = Canonical_Density_Matrix(h_unperturbed, temperature)
@@ -53,37 +60,48 @@ def Simulate(spin_par, zeem_par, quad_par, mode, temperature, pulse_time):
     
 def Energy_Spectrum(h_0):
     energy_spectrum = h_0.eigenvalues()
-    print("Energy eigenvalues of the Zeeman + quadrupole Hamiltonian = %r" % (energy_spectrum), '\n')
+    print("Energy spectrum of the system = %r" % (energy_spectrum), '\n')
     return energy_spectrum
-    
+
+
+def Frequency_Spectrum(energies):
+    transitions = []
+    energies = np.sort(energies)
+    for e1 in energies:
+        for e2 in energies:
+            if e1<e2:
+                transitions.append(np.absolute(e1-e2))
+    print("Spectrum of the transition frequencies = %r" % (transitions), '\n')
+    return transitions
+
 
 # Computes the density matrix of the system after the application of a desired pulse for a given time,
 # given the initial preparation of the ensemble
 def Evolve(spin, dm_0, h_0, mode, T):
     
-    if T == 0: return dm_0
+    if T == 0:
+        return dm_0
     
     print("Initial density matrix = \n %r" % (dm_0.matrix), '\n')
-    
+          
     # Sampling of the time-dependent term of the Hamiltonian representing the coupling with the
     # electromagnetic pulse (already cast in the interaction picture) in the time window [0, T]
     times, time_step = np.linspace(0, T, num=int(T*100), retstep=True)
     h_pulse_ip = []
     for t in times:
         h_pulse_ip.append(H_Pulse_IP(spin, mode, t, h_0))
-        
     # Evaluation of the 1st and 2nd terms of the Magnus expansion for the pulse Hamiltonian in the
     # interaction picture
     magnus_1st = Magnus_Expansion_1st_Term(h_pulse_ip, time_step)
     magnus_2nd = Magnus_Expansion_2nd_Term(h_pulse_ip, time_step)
-    
+
     # Density matrix of the system after evolution under the action of the pulse (in the interaction
     # picture)
     dm_T_ip = dm_0.sim_trans(-(magnus_1st+magnus_2nd), exp=True)
-    
+
     # Evolved density matrix cast back in the Schroedinger picture
     dm_T = dm_T_ip.interaction_picture(h_0, T, invert=True)
-        
+    
     print("Evolved density matrix = \n %r" % (dm_T.matrix), '\n')
     
     return Density_Matrix(dm_T.matrix)
