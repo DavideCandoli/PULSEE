fa5dc7f100a9466e1a9119a1b26038d0456878c4
Davide Candoli <davide.candoli@studio.unibo.it>
2020-07-03 18:48:49 +0200

    Implemented function Partial_Trace, which computes the partial trace of an operator over the desired subspace. The function has been tested in file Test_Many_Body.py.

--- Many_Body.py
+++ Many_Body.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from Operators import Operator, Density_Matrix, Observable
 
+# Returns the tensor product of two operators
 def Tensor_Product_Operator(A, B):
     
     d_A = A.dimension()
@@ -15,7 +16,7 @@ def Tensor_Product_Operator(A, B):
         for j in range(d_A):
             for ii in range(d_B):
                 for jj in range(d_B):
-                    A_tensor_B[i*d_A+ii, j*d_A+jj] = A.matrix[i][j]*B.matrix[ii][jj]
+                    A_tensor_B[i*d_B+ii, j*d_B+jj] = A.matrix[i, j]*B.matrix[ii, jj]
     
     if isinstance(A, Density_Matrix) and isinstance(B, Density_Matrix):
         return Density_Matrix(A_tensor_B)
@@ -25,3 +26,44 @@ def Tensor_Product_Operator(A, B):
     
     else:
         return Operator(A_tensor_B)
+    
+# Returns the partial trace of an operator
+def Partial_Trace(operator, subspace_dimensions, index_position):
+    m = operator.matrix
+    
+    d = subspace_dimensions
+    
+    i = index_position
+    
+    n = len(subspace_dimensions)
+    
+    d_downhill = int(np.prod(d[i+1:n+1]))
+        
+    d_block = d[i]*d_downhill
+        
+    d_uphill = int(np.prod(d[0:i]))
+        
+    partial_trace = np.empty((d_downhill, d_downhill*(d_uphill+1)), dtype=np.ndarray)
+    for j in range(d_uphill):
+        p_t_row = np.empty((d_downhill, d_downhill), dtype=np.ndarray)
+        for k in range(d_uphill):
+            block = m[j*d_block:(j+1)*d_block, k*d_block:(k+1)*d_block]
+            p_t_block = np.zeros((d_downhill, d_downhill))
+            for l in range(d[i]):
+                p_t_block = p_t_block + block[l*d_downhill:(l+1)*d_downhill, \
+                                              l*d_downhill:(l+1)*d_downhill]
+            p_t_row = np.concatenate((p_t_row, p_t_block), axis=1)
+        partial_trace = np.concatenate((partial_trace, p_t_row), axis=0)
+        
+    partial_trace = partial_trace[d_downhill:,d_downhill:]
+    
+    if isinstance(operator, Density_Matrix):
+        return Density_Matrix(partial_trace)
+    
+    elif isinstance(operator, Observable):
+        return Observable(partial_trace)
+    
+    else:
+        return Operator(partial_trace)
+    
+    
--- Test_Many_Body.py
+++ Test_Many_Body.py
@@ -2,13 +2,16 @@ import math
 import numpy as np
 
 from Operators import Operator, Density_Matrix, Observable, \
-                      Random_Density_Matrix
+                      Random_Density_Matrix, \
+                      Random_Operator
 
-from Many_Body import Tensor_Product_Operator
+from Many_Body import Tensor_Product_Operator, Partial_Trace
 
 import hypothesis.strategies as st
-from hypothesis import given, assume
+from hypothesis import given, note
 
+# Checks that the operation implemented by function Tensor_Product_Operator conserves the defining
+# properties of density matrices, i.e. hermitianity, unit trace and positivity
 @given(d = st.integers(min_value=1, max_value=8))
 def test_Tensor_Product_Conserves_Density_Matrix_Properties(d):
     A = Random_Density_Matrix(d)
@@ -23,3 +26,23 @@ def test_Tensor_Product_Conserves_Density_Matrix_Properties(d):
             note("A = %r" % (A.matrix))
             note("B = %r" % (B.matrix))
             raise AssertionError(error_message)
+
+# Checks that the operation implemented by the function Partial_Trace is the inverse of the one performed
+# by Tensor_Product_Operator when they act on unit trace operators
+@given(d = st.integers(min_value=2, max_value=6))
+def test_Partial_Trace_Inverse_Tensor_Product(d):
+    A = Random_Operator(d-1)
+    A = A/A.trace()
+    B = Random_Operator(d)
+    B = B/B.trace()
+    C = Random_Operator(d+1)
+    C = C/C.trace()
+    
+    AB = Tensor_Product_Operator(A, B)
+    BC = Tensor_Product_Operator(B, C)
+    ABC = Tensor_Product_Operator(AB, C)
+    
+    partial_trace = Partial_Trace(ABC, [d-1, d, d+1], 0)
+    
+    assert np.all(np.isclose(partial_trace.matrix, BC.matrix, rtol=1e-10))
+
