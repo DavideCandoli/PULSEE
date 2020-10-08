1a2ffe4a5ddd6a2ccb493071c302a150acc71042
Davide Candoli <davide.candoli@studio.unibo.it>
2020-10-08 12:51:41 +0200

    Added the docstrings to all the functions.

--- Code/Nuclear_Spin.py
+++ Code/Nuclear_Spin.py
@@ -3,12 +3,63 @@ import numpy as np
 
 from Operators import *
 
-# An instance of the following class is to be thought as an all-round representation of the nuclear spin
-# angular momentum. Indeed, it includes all the operators typically associated with the spin and
-# also specific parameters like the spin quantum number and the spin multiplicity
 class Nuclear_Spin:
-
+    """
+    An instance of the following class is to be thought as an all-round representation of the nuclear spin angular momentum. Indeed, it includes all the operators typically associated with the spin and also specific parameters like the spin quantum number and the spin multiplicity.
+    
+    Attributes
+    ----------
+    - quantum_number: float
+                      Half-integer spin quantum number.
+    - d: int
+         Dimensions of the spin Hilbert space.
+    - gyro_ratio_over_2pi: float
+                           Gyromagnetic ratio (over 2 pi) of the nuclear spin measured in units of MHz/T. The gyromagnetic ratio is the constant of proportionality between the intrinsic magnetic moment and the spin angular momentum of a particle.             
+    - I: dict
+         Dictionary whose values are Operator objects representing the cartesian and spherical components of the spin.
+    - I['+']: spin raising operator;
+    - I['-']: spin lowering operator;
+    - I['x']: spin x component;
+    - I['y']: spin y component;
+    - I['z']: spin z component;
+                           
+    Methods
+    -------
+    """
     def __init__(self, s=1, gamma_over_2pi=1):
+        """
+        Constructs an instance of Nuclear_Spin.
+        
+        Parameters
+        ----------
+        - s: float
+             Spin quantum number. The constructor checks if it is a half-integer, and raises appropriate errors in case this condition is not obeyed.
+             Default value is 1;
+        - gamma_over_2pi: float
+                          Gyromagnetic ratio over 2 pi (in units of MHz/T).
+        
+        Action
+        ------
+        Assigns the passed argument s to the attribute quantum_number.
+        Assigns the passed argument gamma to the attribute gyromagnetic_ratio.
+        Initialises the attribute d with the method multiplicity() (see below).
+        Initialises the elements of the dictionary I using the methods described later, according to the following correspondence.
+        |  I  | Method                    |
+        | --- | ------------------------- |
+        |  x  | cartesian_operator()[0]   |
+        |  y  | cartesian_operator()[1]   |
+        |  z  | cartesian_operator()[2]   |
+        |  +  | raising_operator()        |
+        |  -  | lowering_operator()       |
+
+    Returns
+    -------
+    The initialised Nuclear_Spin object.
+    
+    Raises
+    ------
+    ValueError, when the argument s is not a half-integer number (within a relative tolerance of 10^(-10)).
+        """
         s = float(s)
         if not math.isclose(int(2*s), 2*s, rel_tol=1e-10):
             raise ValueError("The given spin quantum number is not a half-integer number")
@@ -23,9 +74,15 @@ class Nuclear_Spin:
         self.gyro_ratio_over_2pi = float(gamma_over_2pi)
     
     def multiplicity(self):
+        """
+        Returns the spin states' multiplicity, namely 2*quantum_number+1 (cast to int).
+        """
         return int((2*self.quantum_number)+1)
 
     def raising_operator(self):
+        """
+        Returns an Operator object representing the raising operator I+ of the spin, expressing its matrix attribute with respect to the basis of the eigenstates of Iz.
+        """
         I_raising = np.zeros((self.d, self.d))
         for m in range(self.d):
             for n in range(self.d):
@@ -34,6 +91,9 @@ class Nuclear_Spin:
         return Operator(I_raising)
 
     def lowering_operator(self):
+        """
+        Returns an Operator object representing the lowering operator I- of the spin, expressing its matrix attribute with respect to the basis of the eigenstates of Iz.
+        """
         I_lowering = np.zeros((self.d, self.d))
         for m in range(self.d):
             for n in range(self.d):
@@ -42,6 +102,15 @@ class Nuclear_Spin:
         return Operator(I_lowering)
 
     def cartesian_operator(self):
+        """
+        Returns a list of 3 Observable objects representing in the order the x, y and z components of the spin. The first two are built up exploiting their relation with the raising and lowering operators (see the formulas above), while the third is simply expressed in its diagonal form, since the chosen basis of representation is made up of its eigenstates.
+        
+        Returns
+        -------
+        - [0]: an Observable object standing for the x component of the spin;
+        - [1]: an Observable object standing for the y component of the spin;
+        - [2]: an Observable object standing for the z component of the spin;
+        """
         I = []
         I.append(Observable(((self.raising_operator() + self.lowering_operator())/2).matrix))
         I.append(Observable(((self.raising_operator() - self.lowering_operator())/(2j)).matrix))
