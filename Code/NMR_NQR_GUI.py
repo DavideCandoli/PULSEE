Changed the algorithm for the calculation of the phase shift in fourier_phase_shift, since the previous one wasn't correct. Now, the calculation is carried out via integration of the Fourier signal.

Code/NMR_NQR_GUI.py
@@ -959,7 +959,7 @@ class NMR_Spectrum(FloatLayout):
     
     error_adj_phase = Label()
     tb_adj_phase = Button()
-
+    
     # Assigns the values of the inputs connected with the acquisition of the FID to the corresponding
     # variables of the Simulation_Manager and generates the FID signal
     def generate_FID(self, sim_man, y_shift, *args):
@@ -991,7 +991,7 @@ class NMR_Spectrum(FloatLayout):
             self.tb_FID.bind(on_release=partial(print_traceback, e))
             self.add_widget(self.tb_FID)
             
-    def calculate_and_plot_fourier(self, sim_man, phase=1):
+    def calculate_and_plot_fourier(self, sim_man):
             self.remove_widget(self.fourier_spectrum)
             plt.close(self.fourier_spectrum_figure)
             
@@ -1004,7 +1004,7 @@ class NMR_Spectrum(FloatLayout):
             if self.input_opposite_frequency == False:
                 sim_man.spectrum_frequencies, \
                 sim_man.spectrum_fourier = fourier_transform_signal(sim_man.FID_times, \
-                                                     phase*sim_man.FID,\
+                                                     sim_man.FID,\
                                                      frequency_start=self.input_frequency_left_bound, \
                                                      frequency_stop=self.input_frequency_right_bound)
                 plot_fourier_transform(sim_man.spectrum_frequencies, sim_man.spectrum_fourier, \
@@ -1013,7 +1013,7 @@ class NMR_Spectrum(FloatLayout):
                 sim_man.spectrum_frequencies, \
                 sim_man.spectrum_fourier, \
                 sim_man.spectrum_fourier_neg = fourier_transform_signal(sim_man.FID_times, \
-                                                     phase*sim_man.FID, \
+                                                     sim_man.FID, \
                                                      frequency_start=self.input_frequency_left_bound, \
                                                      frequency_stop=self.input_frequency_right_bound, \
                                                      opposite_frequency=self.input_opposite_frequency)
@@ -1034,7 +1034,7 @@ class NMR_Spectrum(FloatLayout):
         try:
             self.remove_widget(self.error_fourier)
             self.remove_widget(self.tb_fourier)
-            
+                        
             self.calculate_and_plot_fourier(sim_man)
             
         except Exception as e:
@@ -1051,20 +1051,24 @@ class NMR_Spectrum(FloatLayout):
             self.remove_widget(self.error_adj_phase)
             self.remove_widget(self.tb_adj_phase)
             
-            peak_frequency_hint = float(null_string(self.peak_frequency.text))
+            peak_frequency = float(null_string(self.peak_frequency.text))
         
             search_window = float(null_string(self.search_range.text))
             
             if not self.flip_negative_freq_checkbox.active:
                 phi = fourier_phase_shift(sim_man.spectrum_frequencies, sim_man.spectrum_fourier, \
-                                          peak_frequency_hint=peak_frequency_hint, \
-                                          search_window=search_window)
+                                          peak_frequency=peak_frequency, \
+                                          int_domain_width=search_window)
             else:
                 phi = fourier_phase_shift(sim_man.spectrum_frequencies, sim_man.spectrum_fourier, \
                                           sim_man.spectrum_fourier_neg, \
-                                          peak_frequency_hint, search_window)
+                                          peak_frequency, search_window)
             
-            self.calculate_and_plot_fourier(sim_man, np.exp(1j*phi))
+            print(phi)
+            
+            sim_man.FID = np.exp(1j*phi)*sim_man.FID
+            
+            self.calculate_and_plot_fourier(sim_man)
             
         except Exception as e:
             self.error_adj_phase=Label(text=e.args[0], pos=(225, y_shift+45), size=(100, 100), bold=True, color=(1, 0, 0, 1), font_size='15sp')

Code/Simulation.py
@@ -246,35 +246,32 @@ def fourier_transform_signal(times, signal, frequency_start, frequency_stop, opp
 
 # Finds out the phase responsible for the displacement of the real and imaginary parts of the Fourier
 # spectrum of the FID with respect to the ideal absorptive/dispersive lorentzian shapes
-def fourier_phase_shift(frequencies, fourier, fourier_neg=None, peak_frequency_hint=0, search_window=0.1):
-
-    peak_pos = 0
+def fourier_phase_shift(frequencies, fourier, fourier_neg=None, peak_frequency=0, int_domain_width=0.5):
     
-    # When the Fourier spectrum for the opposite frequencies is passed, the search of the maximum is
-    # carried out in both the positive and negative range of frequencies
     if fourier_neg is not None:
         fourier = np.concatenate((fourier, fourier_neg))
         frequencies = np.concatenate((frequencies, -frequencies))
     
-    # Range where to look for the maximum of the square modulus of the Fourier spectrum
-    search_range = np.nonzero(np.isclose(frequencies, peak_frequency_hint, atol=search_window/2))[0]
+    integration_domain = np.nonzero(np.isclose(frequencies, peak_frequency, atol=int_domain_width/2))[0]
     
-    # Search of the maximum of the square modulus of the Fourier spectrum
-    fourier2_max=0
-    for i in search_range:
-        if (np.absolute(fourier[i])**2)>fourier2_max:
-            fourier2_max = np.absolute(fourier[i])
-            peak_pos = i
-            
-    re = np.real(fourier[peak_pos])
+    int_real_fourier = 0
+    int_imag_fourier = 0
     
-    im = np.imag(fourier[peak_pos])
+    for i in integration_domain:
+        int_real_fourier = int_real_fourier + np.real(fourier[i])
+        int_imag_fourier = int_imag_fourier + np.imag(fourier[i])
     
-    if im >= 0:
-        phase = math.atan(-im/re)
+    atan = math.atan(-int_imag_fourier/int_real_fourier)
+    
+    if np.absolute(int_real_fourier) < 1e-10 :
+        atan = -math.pi/2
+        
+    if int_imag_fourier >= 0:
+        phase = atan + math.pi/2
+    
     else:
-        phase = math.atan(-im/re) + math.pi
-    
+        phase = atan - math.pi/2
+        
     return phase
 
 

Tests/Test_Simulation.py
@@ -278,7 +278,7 @@ def test_two_methods_phase_adjustment():
     t, fid = FID_signal(spin, h_unperturbed, dm_evolved, acquisition_time=500)
     f, fourier0 = fourier_transform_signal(t, fid, 9, 11)
             
-    phi = fourier_phase_shift(f, fourier0, peak_frequency_hint=10)
+    phi = fourier_phase_shift(f, fourier0, peak_frequency=10, int_domain_width=1)
     f, fourier1 = fourier_transform_signal(t, np.exp(1j*phi)*fid, 9, 11)
             
     t, fid_rephased = FID_signal(spin, h_unperturbed, dm_evolved, acquisition_time=500, phi=-phi)