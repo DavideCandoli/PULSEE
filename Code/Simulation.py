Changed the implementation of fourier_phase_shift, since the old algorithm could lead to wrong results. Indeed, if the real integral was already close to 0 before the phase correction, then the final NMR spectrum could exhibit an imaginary part changed by sign.

Code/Simulation.py
@@ -727,12 +727,16 @@ def fourier_phase_shift(frequencies, fourier, fourier_neg=None, peak_frequency=0
     for i in integration_domain:
         int_real_fourier = int_real_fourier + np.real(fourier[i])
         int_imag_fourier = int_imag_fourier + np.imag(fourier[i])
+        
+    if np.absolute(int_real_fourier) < 1e-10 :
+        if int_imag_fourier > 0:
+            return 0
+        else:
+            return math.pi
     
     atan = math.atan(-int_imag_fourier/int_real_fourier)
     
-    if np.absolute(int_real_fourier) < 1e-10 :
-        phase = 0
-    elif int_real_fourier > 0:
+    if int_real_fourier > 0:
         phase = atan + math.pi/2
     else:
         phase = atan - math.pi/2